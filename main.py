cf30e729b2acb27e42cecbc091a3f74bbd96ecae
Johararjan <83404803+Johararjan@users.noreply.github.com>
2022-03-14 20:54:32 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import typer
+import smtplib
+app=typer.Typer()
+
+@app.command()
+def enter(sender: str, receivers:str, password:str, subject:str, body:str):
+            #in the above input prompt enter your google account application specific password for more details please read the
+            # readme.txt file
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            message = f'subject:{subject}\n\n{body}'
+            server.starttls()
+            server.login(sender, password)
+            print('login successfull')
+            server.sendmail(sender, receivers, message)
+            print("email sent successfully")
+
+
+if __name__=="__main__":
+    app()
